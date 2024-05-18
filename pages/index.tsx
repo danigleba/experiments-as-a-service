18a890ffde84cbd50414d1e86a143877725878b7
Dani Gleba <daniglebapuig@gmail.com>
2024-05-18 14:09:34 +0200

    new copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import { Inter } from "next/font/google"
 import React from "react"
 import dynamic from "next/dynamic"
@@ -12,20 +13,20 @@ export default function Home() {
   const router = useRouter()
   const globeConfig = {
     pointSize: 4,
-    globeColor: "#062056",
+    globeColor: "#30313D",
     showAtmosphere: true,
-    atmosphereColor: "#FFFFFF",
+    atmosphereColor: "#f9f9f9f",
     atmosphereAltitude: 0.1,
     emissive: "#062056",
     emissiveIntensity: 0.1,
     shininess: 0.9,
     polygonColor: "rgba(255,255,255,0.7)",
     ambientLight: "#38bdf8",
-    directionalLeftLight: "#ffffff",
-    directionalTopLight: "#ffffff",
-    pointLight: "#ffffff",
-    arcTime: 1000,
-    arcLength: 0.9,
+    directionalLeftLight: "#f9f9f9f",
+    directionalTopLight: "#f9f9f9f",
+    pointLight: "#f9f9f9f",
+    arcTime: 900,
+    arcLength: 0.5,
     rings: 1,
     maxRings: 3,
     initialPosition: { lat: 22.3193, lng: 114.1694 },
@@ -324,28 +325,53 @@ export default function Home() {
     },
   ]
   return (
-    <main className={`flex flex-col md:flex-row md:items-center md:justify-between fixed w-screen h-screen overflow-hidden text-[#30313D] ${inter.className}`}>
-      <div className="md:absolute top-0 flex items-center justify-start w-full py-6 md:py-12 px-6 md:px-12">
-        <p className="text-xl font-bold">Portiko</p>
-      </div>
-      <div className="z-50 flex flex-col items-start justify-start md:w-1/2 text-left w-full px-6 text-center md:text-left mt-6 md:mt-0 md:mt-0 md:pl-12">
-        <div className="space-y-6 md:space-y-12">
-          <div className="space-y-6">
-            <p className="font-black text-3xl md:text-6xl leading-12">Find scientific papers to collaborate on, in minutes</p>
-            <p className="text-sm md:text-base">Portiko connects researchers who are looking for external collaborators with researchers who, after gaining experience in a series of experiments or analyses, are open to collaborate with laboratories that need them.</p>
-          </div>
-          <div className="flex flex-col md:flex-row items-center justify-start gap-6 pt-6 z-50">
-            <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfPoDhNKdcui13eF1pG3MWXKJ4IqH1UjJ_ogmvPYTHyKbHQ8g/viewform?usp=sf_link" className="bg-[#30313D]">Collaborate on papers</a>
-            <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdQq0ZjPJEn3ZiT-L49AKhd6upliBvka-HaMdhuMsr44O2i3w/viewform?usp=sf_link" className="border border-[#30313D] text-[#30313D]">Find collaborators</a>
-          </div>
-        </div> 
-      </div>
-      <div className="hidden md:flex md:fixed bottom-0 md:top-0 w-full aspect-square md:h-full md:ml-96 md:p-24 md:pr-46 md:pt-24">
-        <World data={sampleArcs} globeConfig={globeConfig} />
-      </div>
-      <div className="md:hidden absolute md:static bottom-0 md:w-1/2 w-full h-3/4 md:h-full z-10 -mb-72 md:-mb-0 md:p-24">
-          <World data={sampleArcs} globeConfig={globeConfig} />
+    <>
+      <Head>
+        {/* Basic Meta Tags */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Portiko | Find scientific collaborations in minutes"/>
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content="Portiko" />
+        <meta property="og:description" content="Portiko | Find scientific collaborations in minutes" />
+        <meta property="og:image" content="/icon.png" />
+        <meta property="og:url" content="portiko.vercel.app" />
+        <meta property="og:type" content="website" />
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Portiko" />
+        <meta name="twitter:description" content="Portiko | Find scientific collaborations in minutes" />
+        <meta name="twitter:image" content="/icon.png" />
+        {/* Favicon */}
+        <link rel="icon" href="/icon.png" />
+        {/* Page Title */}
+        <title>Portiko | Find scientific collaborations in minutes</title>
+      </Head>
+      <main className={`flex flex-col md:flex-row md:items-center md:justify-between fixed w-screen h-screen overflow-hidden text-[#30313D] ${inter.className}`}>
+        <div className="md:absolute top-0 flex items-center justify-start w-full py-6 md:py-12 px-6 md:px-24">
+          <p className="text-xl font-bold">Portiko</p>
         </div>
-    </main>
+        <div className="flex items-start md:items-center grid-cols-2 h-2/3">
+          <div className="z-50 flex flex-col items-start justify-start md:w-full text-left w-full px-6 text-center md:text-left mt-6 md:mt-0 md:mt-0 md:pl-24">
+            <div className="space-y-6 md:space-y-12">
+              <div className="space-y-6">
+                <p className="font-black text-3xl md:text-6xl leading-12">Find scientific collaborations in minutes</p>
+                <p className="text-sm md:text-base">Portiko connects scientists looking to carry out experiments or analysis and researchers with the experience and tools to do so.</p>
+              </div>
+              <div className="flex flex-col md:flex-row items-center justify-start gap-6 pt-6 z-50">
+                <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfPoDhNKdcui13eF1pG3MWXKJ4IqH1UjJ_ogmvPYTHyKbHQ8g/viewform?usp=sf_link" className="bg-[#30313D] border border-[#30313D] hover:bg-[#232323] duration-200 ease-in-out">Find collaborations</a>
+                <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSdQq0ZjPJEn3ZiT-L49AKhd6upliBvka-HaMdhuMsr44O2i3w/viewform?usp=sf_link" className="border border-[#30313D] text-[#30313D] hover:bg-[#f9f9f9] duration-200 ease-in-out">Offer collaborations</a>
+              </div>
+            </div> 
+          </div>
+          <div className="hidden md:flex md:top-0 w-full h-full">
+            <World data={sampleArcs} globeConfig={globeConfig} />
+          </div>
+        </div>
+        <div className="md:hidden absolute md:static bottom-0 md:w-1/2 w-full h-3/4 md:h-full z-10 -mb-72 md:-mb-0 md:p-24">
+            <World data={sampleArcs} globeConfig={globeConfig} />
+          </div>
+      </main>
+    </>
   )
 }
